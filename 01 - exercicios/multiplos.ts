0c1d549d6aabc9a5015879ac7fbc6e8099e4e7b7
Marcos Victor <marcosvictorsb@gmail.com>
2021-11-04 18:33:23 -0300

    trocado o nome do metodo

--- 01 - exercicios/multiplos.ts
+++ 01 - exercicios/multiplos.ts
@@ -1,5 +1,11 @@
-export default function sumNaturaisNumbers(value: number): number {
-      
+export default function sumNaturalNumbersBelow(value: number): number {
+    let result = 0
+    for(let i = 0; i < value; i++){
+        const divisible3Or5 = i % 3 == 0 || i % 5 == 0
+        if(divisible3Or5){
+            result += i;
+        }
+    }
     
-    return 23
+    return result
 }
--- test/multiplos.test.ts
+++ test/multiplos.test.ts
@@ -1,9 +1,9 @@
-import sumNaturaisNumbers from '../01 - exercicios/multiplos'
+import sumNaturalNumbersBelow from '../01 - exercicios/multiplos'
 
 test('A soma dos numeros naturais abaixo de 10 divisivel por 3 ou 5', () => {
     const expectedValue = 23
 
-    const result = sumNaturaisNumbers(10)
+    const result = sumNaturalNumbersBelow(10)
 
     expect(expectedValue).toEqual(result)
 })
