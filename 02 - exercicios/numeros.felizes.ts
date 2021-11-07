9ebb14fb70eaa71b502ba10320d56a952f1e7fc3
Marcos Victor <marcosvictorsb@gmail.com>
2021-11-07 17:22:38 -0300

    Refatorações

--- 02 - exercicios/numeros.felizes.ts
+++ 02 - exercicios/numeros.felizes.ts
@@ -1,12 +1,36 @@
-export default function numberHappy(value: number, arr: Array<number> = []): string {
+
+export function numbersSplits(value: number): number[] {
     
     let digitSplit = value.toString().split("")
-    let digits = digitSplit.map(Number)
+    return digitSplit.map(Number)
+}
 
-    let sum = digits
-                .map(number => number * number)
-                .reduce((number, accumulator) => number + accumulator, 0)
+export function squareSumOfNumbers(value: number[]): number {
+    return value
+        .map(number => number * number)
+        .reduce((number, accumulator) => number + accumulator, 0)
+}
+
+export function thisNumberExistInArray(value: number, arr: number[]) : boolean {
+    return arr.some(number => number === value)
+}
+
+/*
+export function isNumberHappy(sum: number, arr: number[]): any {
+    if(sum !== 1){
+        const result = thisNumberExistInArray(sum, arr)  
+        arr.push(sum)
+        return result ? false : numberHappy(sum, arr)
+    }
     
+    return true
+}
+*/
+export function numberHappy(value: number, arr: Array<number> = []): string {    
+    
+    let digits = numbersSplits(value)
+    let sum = squareSumOfNumbers(digits)
+    // return isNumberHappy(sum, arr) ? "Numero feliz" : "Numero não é feliz"
     if(sum !== 1){
         const result = arr.some(number => number === sum)
         arr.push(sum)
--- test/numeros.felizes.test.ts
+++ test/numeros.felizes.test.ts
@@ -1,4 +1,4 @@
-import numberHappy from '../02 - exercicios/numeros.felizes'
+import { numberHappy, numbersSplits, squareSumOfNumbers, thisNumberExistInArray} from '../02 - exercicios/numeros.felizes'
 
 test('Quando verificar o numero 7 deve um retornar "Numero feliz"', () => {
     const expectedValue = 'Numero feliz'
@@ -12,6 +12,54 @@ test('Quando verificar o numero 2 deve me retonar "Numero não feliz"' , () => {
     const expectedValue = 'Numero não é feliz'
 
     const result = numberHappy(2)
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna um numero 15 separado em um array [1, 5]', () => {
+    const expectedValue  = [1, 5]
+
+    const result = numbersSplits(15)
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna um numero 155 separado em um array [1, 5, 5]', () => {
+    const expectedValue  = [1, 5, 5]
+
+    const result = numbersSplits(155)
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna a soma do quadrado dos digitos, informando os digitos 9 e 7 deve retornar 130', () => {
+    const expectedValue = 130
+
+    const result = squareSumOfNumbers([9, 7])
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna a soma do quadrado dos digitos, informando os digitos 4 e 9 deve retornar 97', () => {
+    const expectedValue = 97
+
+    const result = squareSumOfNumbers([4, 9])
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna verdadeiro ao verificar se o numero 5 existe neste array [1, 2, 3, 4, 5]', () => {
+    const expectedValue = true
+
+    const result = thisNumberExistInArray(5, [1, 2, 3, 4, 5])
+
+    expect(expectedValue).toEqual(result)
+})
+
+test('Retorna falso ao verificar se o numero 5 existe neste array [1, 2, 3, 4]', () => {
+    const expectedValue = false
+
+    const result = thisNumberExistInArray(5, [1, 2, 3, 4])
 
     expect(expectedValue).toEqual(result)
 })
