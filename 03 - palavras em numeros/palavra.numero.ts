c7dd0f2e814e91b8d60cc9fe242c157d102adddd
Marcos Victor <marcosvictorsb@gmail.com>
2021-11-07 18:41:03 -0300

    teste para retornar o valor da soma de uma palavra. Se é primo ou não. Se é multiplo de 3 ou 5

--- 03 - palavras em numeros/palavra.numero.ts
+++ 03 - palavras em numeros/palavra.numero.ts
@@ -1,5 +1,38 @@
+import { numberHappy } from "../02 - exercicios/numeros.felizes";
+// import {sumNaturalNumbersDivisible3Or5Below} from "../01 - exercicios/multiplos"
+
 const letras = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
 export function valueWord(value: string): number {
-    return letras.indexOf(value) + 1
+    
+    let result = 0
+    for (let i = 0; i < value.length; i++) {
+        result += letras.indexOf(value[i]) + 1
+        
+    }
+    return result
+}
+
+export function numbersOfDividers(value: number): number{
+    let count = 0
+    for (let i = 1; i <= value; i++) {
+        if(value % i === 0){
+            count += 1
+        } 
+    }
+    return count
+
+}
+
+export function isPrimeNumber(value: number): string {
+    const count = numbersOfDividers(value)
+    if(count === 2){
+        return "Primo"
+    }
+    return "Não primo"
+
+}
+
+export function isDivisible3Or5(value: number): boolean {
+    return value % 3 === 0 || value % 5 === 0
 }
--- test/palavra.numero.test.ts
+++ test/palavra.numero.test.ts
@@ -1,51 +1,141 @@
-import {valueWord} from '../03 - palavras em numeros/palavra.numero'
+import {valueWord, isPrimeNumber, numbersOfDividers, isDivisible3Or5} from '../03 - palavras em numeros/palavra.numero'
 
-test('Informando a letra "a" teria a resposta deve ser 1', () => {
-    const expectedValue = 1
-
-    const result = valueWord('a')
-
-    expect(expectedValue).toEqual(result)
+describe('Valor de uma letra informanda', () => {
+    test('Informando a letra "a" teria a resposta deve ser 1', () => {
+        const expectedValue = 1
+    
+        const result = valueWord('a')
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    test('Informando a letra "b" teria a resposta deve ser 2', () => {
+        const expectedValue = 2
+    
+        const result = valueWord('b')
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    test('Informando a letra "z" teria a resposta deve ser 26', () => {
+        const expectedValue = 26
+    
+        const result = valueWord('z')
+    
+        expect(expectedValue).toEqual(result)
+    })    
+    
+    test('Informando a letra "A" teria a resposta deve ser 27', () => {
+        const expectedValue = 27
+    
+        const result = valueWord('A')
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    test('Informando a letra "B" teria a resposta deve ser 28', () => {
+        const expectedValue = 28
+    
+        const result = valueWord('B')
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    test('Informando a letra "Z" teria a resposta deve ser 52', () => {
+        const expectedValue = 52
+    
+        const result = valueWord('Z')
+    
+        expect(expectedValue).toEqual(result)
+    })
 })
 
-test('Informando a letra "b" teria a resposta deve ser 2', () => {
-    const expectedValue = 2
-
-    const result = valueWord('b')
-
-    expect(expectedValue).toEqual(result)
+describe('Informando uma palavra dever retornar a soma dos valores', () => {
+    test('Informando a palavras "marcos" a resposata deve ser 69', () => {
+        const expectedValue = 69
+    
+        const result = valueWord('marcos')
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    test('Informando a palavras "marcos2548" a resposata deve ser 69', () => {
+        const expectedValue = 69
+    
+        const result = valueWord('marcos2548')
+    
+        expect(expectedValue).toEqual(result)
+    })    
 })
 
-test('Informando a letra "z" teria a resposta deve ser 26', () => {
-    const expectedValue = 26
+describe('Primo ou Não', () => {
+    test('Informando o numero 5 deve retornar que "Primo"', () => {
+        const expectedValue = "Primo"
+    
+        const result = isPrimeNumber(5)
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+    
+    test('Informando o numero 4 deve retornar que "Não primo"', () => {
+        const expectedValue = "Não primo"
+    
+        const result = isPrimeNumber(4)
+    
+        expect(expectedValue).toEqual(result)
+    })
+    
+})
 
-    const result = valueWord('z')
+describe('Quantidade de divisores', () => {
+    test('Informando o numero 10 o numero de divisores deve ser igual a 4', () => {
+        const expectedValue = 4
+    
+        const result = numbersOfDividers(10)
+    
+        expect(expectedValue).toEqual(result)
+    
+    })
+})
 
-    expect(expectedValue).toEqual(result)
+describe('Multiplo de 3 ou 5', () => {
+    it('Informando o numero 15 deve retornar "true"', () => {
+        const expectedValue = true
+
+        const result = isDivisible3Or5(15)
+
+        expect(expectedValue).toEqual(result)
+    })
+
+    it('Informando o numero 9 deve retornar "true"', () => {
+        const expectedValue = true
+
+        const result = isDivisible3Or5(9)
+
+        expect(expectedValue).toEqual(result)
+    })
+
+    it('Informando o numero 10 deve retornar "true"', () => {
+        const expectedValue = true
+
+        const result = isDivisible3Or5(10)
+
+        expect(expectedValue).toEqual(result)
+    })
+
+    it('Informando o numero 11 deve retornar "false"', () => {
+        const expectedValue = false
+
+        const result = isDivisible3Or5(11)
+
+        expect(expectedValue).toEqual(result)
+    })
 })
 
 
-test('Informando a letra "A" teria a resposta deve ser 27', () => {
-    const expectedValue = 27
-
-    const result = valueWord('A')
-
-    expect(expectedValue).toEqual(result)
+describe('Prima ou Não - Feliz ou Não - Multiplo de 3 ou 5', () => {
+    // test('infromando o numero 2 deve retornar "Primo - Não Feliz "')
 })
 
-test('Informando a letra "B" teria a resposta deve ser 28', () => {
-    const expectedValue = 28
-
-    const result = valueWord('B')
-
-    expect(expectedValue).toEqual(result)
-})
-
-test('Informando a letra "Z" teria a resposta deve ser 52', () => {
-    const expectedValue = 52
-
-    const result = valueWord('Z')
-
-    expect(expectedValue).toEqual(result)
-})
 
